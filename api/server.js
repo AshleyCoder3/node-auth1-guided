df8457d5cd547953f80ad3abb545af7eda00bb4e
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-09-13 19:24:00 +0200

    wip

--- api/server.js
+++ api/server.js
@@ -12,7 +12,15 @@ server.use(express.static(path.join(__dirname, '../client')));
 // server.use(helmet()); // fewer headers
 server.use(express.json());
 server.use(session({
-
+  name: 'notsession', // default is connect.sid
+  secret: 'nobody tosses a dwarf!',
+  cookie: {
+    maxAge: 1 * 24 * 60 * 60 * 1000,
+    secure: true, // only set cookies over https. Server will not send back a cookie over http.
+  }, // 1 day in milliseconds
+  httpOnly: true, // don't let JS code access cookies. Browser extensions run JS code on your browser!
+  resave: false,
+  saveUninitialized: false,
 }));
 
 server.use('/api/users', usersRouter);
