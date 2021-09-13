77b3b0e710726709da705590a44ea70c92415084
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-09-13 19:49:16 +0200

    wip

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -6,11 +6,11 @@ const protected = (req, res, next) => {
   if (req.session.user) {
     next()
   } else {
-    next({ status: 401, ''})
+    next({ status: 401, message: 'you must be authed to see this' })
   }
 }
 
-router.get("/", (req, res, next) => { // we will protect this one!!!!
+router.get("/", protected, (req, res, next) => { // we will protect this one!!!!
   Users.find()
     .then(users => {
       res.status(200).json(users);
