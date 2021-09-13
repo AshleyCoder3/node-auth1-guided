8741dce9dc7242113859d76eec88bd4b84eb1468
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-09-13 19:48:29 +0200

    wip

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -2,6 +2,14 @@ const router = require("express").Router();
 
 const Users = require("./users-model.js");
 
+const protected = (req, res, next) => {
+  if (req.session.user) {
+    next()
+  } else {
+    next({ status: 401, ''})
+  }
+}
+
 router.get("/", (req, res, next) => { // we will protect this one!!!!
   Users.find()
     .then(users => {
