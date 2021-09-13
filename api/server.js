c7382cf05efae171e32d84fe287287fbb74d31ab
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-09-13 18:33:11 +0200

    wip

--- api/server.js
+++ api/server.js
@@ -8,7 +8,7 @@ const authRouter = require('./auth/auth-router');
 const server = express();
 
 server.use(express.static(path.join(__dirname, '../client')));
-server.use(helmet());
+// server.use(helmet()); // fewer headers
 server.use(express.json());
 
 server.use('/api/users', usersRouter);
