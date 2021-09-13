017e84fa4ea19ffe3064240a479bbc2120f38085
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-09-13 19:07:57 +0200

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -3,21 +3,28 @@ const User = require('../users/users-model')
 const bcrypt = require('bcryptjs')
 
 router.post('/register', async (req, res, next) => {
-  const { username, password } = req.body
-  const hash = bcrypt.hashSync(password, 8) // 2 ^ 8
-  const newUser = { username, password: hash }
+  try {
+    const { username, password } = req.body
+    const hash = bcrypt.hashSync(password, 8) // 2 ^ 8
+    const newUser = { username, password: hash }
+    const user = await User.add(newUser)
 
-  const user = await User.add(newUser)
-
-  res.status(201).json(user)
+    res.status(201).json(user)
+  } catch (err) {
+    next(err)
+  }
 })
 
 router.post('/login', async (req, res, next) => {
-  const { username, password } = req.body
-  // check if username in db
-  // recreate hash from password
-  // if username exists, AND hash matches the one in db
-  // THEN START A SESSION WITH THE HELP OF A LIB expresse-session
+  try {
+    const { username, password } = req.body
+    // check if username in db
+    // recreate hash from password
+    // if username exists, AND hash matches the one in db
+    // THEN START A SESSION WITH THE HELP OF A LIB expresse-session
+  } catch (err) {
+    next(err)
+  }
 })
 
 router.get('/logout', async (req, res, next) => {
