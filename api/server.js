f98d659a63efedc2f5a5204e1f0d562727ce9219
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-09-13 20:09:08 +0200

    wip

--- api/server.js
+++ api/server.js
@@ -22,6 +22,14 @@ server.use(session({
   httpOnly: true, // don't let JS code access cookies. Browser extensions run JS code on your browser!
   resave: false, // IGNORE, some libs need this
   saveUninitialized: false, // only save a session if user approves
+  // PERSISTING SESSIONS TO THE DATABASE
+  store: new store({
+    knex: require('../database/db-config.js'), // configured instance of knex
+    tablename: 'sessions', // table that will store sessions inside the db, name it anything you want
+    sidfieldname: 'sid', // column that will hold the session id, name it anything you want
+    createtable: true, // if the table does not exist, it will create it automatically
+    clearInterval: 1000 * 60 * 60, // time it takes to check for old sessions and remove them from the database to keep it clean and performant
+  }),
 }));
 
 server.use('/api/users', usersRouter);
